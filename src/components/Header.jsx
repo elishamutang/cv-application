51f9f1e37392e322852306786d9e7d0310d66dbb
elisha <elishamutang@outlook.com>
2024-09-16 23:41:20 +0800

    [update] Updated header component to include user details.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,15 +2,40 @@ import { useState } from "react";
 
 function Header() {
   const [fullName, setFullName] = useState("John Doe");
+  const [contactDetails, setContactDetails] = useState([
+    { id: 0, field: "Street Address" },
+    { id: 1, field: "City, State Zip" },
+    { id: 2, field: "youremail@contact.com" },
+    { id: 3, field: "Phone Number" },
+  ]);
 
   function handleName(e) {
     setFullName(e.target.value);
   }
 
+  function handleContactChange(e) {
+    setContactDetails(
+      contactDetails.map((item) => {
+        if (item.id == e.target.id) {
+          return { ...item, field: e.target.value };
+        } else {
+          return item;
+        }
+      })
+    );
+  }
+
   return (
-    <div id="name">
-      <input type="text" id="fullName" value={fullName} onChange={handleName} />
-    </div>
+    <>
+      <div id="name">
+        <input type="text" id="fullName" value={fullName} onChange={handleName} />
+      </div>
+      <div id="contact">
+        {contactDetails.map((item) => {
+          return <input key={item.id} id={item.id} value={item.field} onChange={handleContactChange} />;
+        })}
+      </div>
+    </>
   );
 }
 
