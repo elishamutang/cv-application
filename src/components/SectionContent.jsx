6ef9eadba4b7b85dcb20e4b4848c214e0776c08a
elisha <elishamutang@outlook.com>
2024-09-17 21:40:06 +0800

    [update] Split the content and section title from Section.jsx.

---
+++ src/components/SectionContent.jsx
@@ -0,0 +1,30 @@
+function SectionContent() {
+  return (
+    <>
+      <div className="company-position">
+        <p>Organisation</p>
+        <p>Position Title</p>
+      </div>
+      <div className="location-duration">
+        <p>City, State</p>
+        <p>MMM YYYY - MMM YYYY</p>
+      </div>
+      <div className="content">
+        <ul>
+          <li>
+            Beginning with most recent position, describe your experience, skills, and resulting outcomes in bullet
+            form.
+          </li>
+          <li>
+            Begin each line with an action verb and include details that will help the reader understand your
+            accomplishments, skills, knowledge, abilities, or achievements.
+          </li>
+          <li>Quantify where possible.</li>
+          <li>Do not use personal pronouns; each line should be a phrase rather than a full sentence.</li>
+        </ul>
+      </div>
+    </>
+  );
+}
+
+export default SectionContent;
