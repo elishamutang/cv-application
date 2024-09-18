64f536b3b61526a11a2d0f1199197a8bd7c4ed8e
elisha <elishamutang@outlook.com>
2024-09-18 21:23:42 +0800

    [update] Completed half of this component.

--- src/components/SectionContent.jsx
+++ src/components/SectionContent.jsx
@@ -5,13 +5,16 @@ function SectionContent() {
   const [defaultValues, setValues] = useState({
     organisation: "Organisation",
     position: "Position Title",
-    city: "City",
-    state: "State",
-    from: "MMM YYYY",
-    to: "MMM YYYY",
+    location: "City, State",
+    startDate: "MMM YYYY",
+    endDate: "MMM YYYY",
   });
 
-  const text = useRef("");
+  const organisationText = useRef("");
+  const positionTitleText = useRef("");
+  const cityAndState = useRef("");
+  const startDate = useRef("");
+  const endDate = useRef("");
 
   function handleOrganisationChange(e) {
     setValues({ ...defaultValues, organisation: e.target.value });
@@ -22,35 +25,52 @@ function SectionContent() {
   }
 
   function handleBlur() {
-    console.log(text.current.innerHTML);
+    console.log(organisationText.current.innerHTML);
+    console.log(positionTitleText.current.innerHTML);
+    console.log(cityAndState.current.innerHTML);
   }
 
   return (
     <>
       <div className="company-position">
-        {/* Fix these components */}
         <ContentEditable
-          innerRef={text}
+          innerRef={organisationText}
           html={defaultValues.organisation}
           onBlur={handleBlur}
           onChange={handleOrganisationChange}
         />
         <ContentEditable
-          innerRef={text}
+          innerRef={positionTitleText}
           html={defaultValues.position}
           onBlur={handleBlur}
           onChange={handlePositionChange}
         />
       </div>
       <div className="location-duration">
-        <p>
-          {defaultValues.city}, {defaultValues.state}
-        </p>
-        <p>
-          {defaultValues.from} - {defaultValues.to}
-        </p>
+        <ContentEditable
+          innerRef={cityAndState}
+          html={defaultValues.location}
+          onBlur={handleBlur}
+          onChange={(e) => setValues({ ...defaultValues, location: e.target.value })}
+        />
+        <div className="duration">
+          <ContentEditable
+            innerRef={startDate}
+            html={defaultValues.startDate}
+            onBlur={handleBlur}
+            onChange={(e) => setValues({ ...defaultValues, startDate: e.target.value })}
+          />
+          -
+          <ContentEditable
+            innerRef={endDate}
+            html={defaultValues.endDate}
+            onBlur={handleBlur}
+            onChange={(e) => setValues({ ...defaultValues, endDate: e.target.value })}
+          />
+        </div>
       </div>
       <div className="content">
+        {/* Needs to be changed to content editable elements */}
         <ul>
           <li>
             Beginning with most recent position, describe your experience, skills, and resulting outcomes in bullet
