6280dbd4599c28a9909d1d83851ff84f584bdeca
elisha <elishamutang@outlook.com>
2024-09-20 21:27:00 +0800

    [update] Separated Education component into two functions.

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -1,12 +1,41 @@
-import { useState } from "react";
+import { useState, useRef, Fragment } from "react";
+import ContentEditable from "react-contenteditable";
 
-function Education() {
+function EducationSection() {
+  const [defaultEducation, setEducation] = useState([
+    { id: 0, university: "Example University", location: "Cambridge, MA", graduationDate: "DD/MM/YYYY" },
+  ]);
+
+  const university = useRef("");
+
+  function handleUniversityChange(e, itemId) {
+    setEducation(
+      defaultEducation.map((item) => {
+        if (item.id === itemId) {
+          return { ...defaultEducation, university: e.target.value };
+        } else {
+          return defaultEducation;
+        }
+      })
+    );
+  }
+
   return (
-    <>
-      <div className="title">Education</div>
-      <div className="institution">Harvard University</div>
+    <div className="education-section">
+      {defaultEducation.map((item) => {
+        return (
+          <Fragment key={item.id}>
+            <ContentEditable
+              innerRef={university}
+              html={item.university}
+              className="institution"
+              onChange={(e) => handleUniversityChange(e, item.id)}
+            />
+          </Fragment>
+        );
+      })}
       <div className="location-duration">
-        <p>Cambridge, MA</p>
+        {/* <ContentEditable html={}/> */}
         <p>Graduation Date</p>
       </div>
       <div className="content">
@@ -16,7 +45,16 @@ function Education() {
           <li>Relevant Coursework: [Note: Optional. Awards and honors can also be listed here]</li>
         </ul>
       </div>
-    </>
+    </div>
+  );
+}
+
+function Education() {
+  return (
+    <div id="education">
+      <div className="title">Education</div>
+      <EducationSection />
+    </div>
   );
 }
 
