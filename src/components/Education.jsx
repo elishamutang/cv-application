9ed31a23fac2f47cb923fc06142a2798f994acff
elisha <elishamutang@outlook.com>
2024-09-20 21:57:29 +0800

    [update] Removed useRef.

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -1,41 +1,12 @@
-import { useState, useRef, Fragment } from "react";
+import { useState } from "react";
 import ContentEditable from "react-contenteditable";
 
-function EducationSection() {
-  const [defaultEducation, setEducation] = useState([
-    { id: 0, university: "Example University", location: "Cambridge, MA", graduationDate: "DD/MM/YYYY" },
-  ]);
-
-  const university = useRef("");
-
-  function handleUniversityChange(e, itemId) {
-    setEducation(
-      defaultEducation.map((item) => {
-        if (item.id === itemId) {
-          return { ...defaultEducation, university: e.target.value };
-        } else {
-          return defaultEducation;
-        }
-      })
-    );
-  }
-
+function EducationSection({ education, handleUniversityChange, className }) {
   return (
     <div className="education-section">
-      {defaultEducation.map((item) => {
-        return (
-          <Fragment key={item.id}>
-            <ContentEditable
-              innerRef={university}
-              html={item.university}
-              className="institution"
-              onChange={(e) => handleUniversityChange(e, item.id)}
-            />
-          </Fragment>
-        );
-      })}
+      <ContentEditable html={education.university} className={className} onChange={handleUniversityChange} />
       <div className="location-duration">
-        {/* <ContentEditable html={}/> */}
+        <ContentEditable html={education.location} />
         <p>Graduation Date</p>
       </div>
       <div className="content">
@@ -50,10 +21,33 @@ function EducationSection() {
 }
 
 function Education() {
+  const [defaultEducation, setEducation] = useState([
+    { id: 0, university: "Example University", location: "Cambridge, MA", graduationDate: "DD/MM/YYYY" },
+  ]);
+
+  function handleUniversityChange(e, itemId) {
+    setEducation(
+      defaultEducation.map((item) => {
+        if (item.id === itemId) {
+          return { ...defaultEducation, university: e.target.value };
+        } else {
+          return defaultEducation;
+        }
+      })
+    );
+  }
+
   return (
     <div id="education">
       <div className="title">Education</div>
-      <EducationSection />
+      {defaultEducation.map((item) => (
+        <EducationSection
+          key={item.id}
+          education={item}
+          onChange={(e) => handleUniversityChange(e, item.id)}
+          className="institution"
+        />
+      ))}
     </div>
   );
 }
