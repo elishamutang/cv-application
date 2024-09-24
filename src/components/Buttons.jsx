6ca3cc6ba2bd73119dd0405e27f11841542b81bc
elisha <elishamutang@outlook.com>
2024-09-24 23:41:20 +0800

    [update] Added additional buttons.

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,6 +1,6 @@
-function RemoveSection({ handleRemove }) {
+function RemoveSection({ handleRemoveSectionContent }) {
   return (
-    <button onClick={handleRemove} className="removeBtn">
+    <button onClick={handleRemoveSectionContent} className="removeBtn">
       Remove
     </button>
   );
@@ -14,4 +14,26 @@ function AddSection({ sectionName, handleClick }) {
   );
 }
 
-export { RemoveSection, AddSection };
+function IcBaselineRemoveCircle(props) {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" {...props}>
+      <path
+        fill="currentColor"
+        d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10s10-4.48 10-10S17.52 2 12 2m5 11H7v-2h10z"
+      ></path>
+    </svg>
+  );
+}
+
+function IcRoundAddCircle(props) {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" {...props}>
+      <path
+        fill="currentColor"
+        d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10s10-4.48 10-10S17.52 2 12 2m4 11h-3v3c0 .55-.45 1-1 1s-1-.45-1-1v-3H8c-.55 0-1-.45-1-1s.45-1 1-1h3V8c0-.55.45-1 1-1s1 .45 1 1v3h3c.55 0 1 .45 1 1s-.45 1-1 1"
+      ></path>
+    </svg>
+  );
+}
+
+export { RemoveSection, AddSection, IcBaselineRemoveCircle, IcRoundAddCircle };
