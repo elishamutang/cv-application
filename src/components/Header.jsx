920265bdc44597308541f1b2dd7e82a429633278
elisha <elishamutang@outlook.com>
2024-10-09 20:59:34 +0800

    [update] Implemented local storage.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,27 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function Header() {
-  const [fullName, setFullName] = useState("John Doe");
-  const [contactDetails, setContactDetails] = useState([
+  const [fullName, setFullName] = useState(() => {
+    const checkLocal = localStorage.getItem("name");
+    return checkLocal ? checkLocal : "John Doe";
+  });
+
+  const initialContactDetails = [
     { id: 0, field: "Street Address" },
     { id: 1, field: "City, State Zip" },
     { id: 2, field: "youremail@contact.com" },
     { id: 3, field: "Phone Number" },
-  ]);
+  ];
+
+  const [contactDetails, setContactDetails] = useState(() => {
+    const checkLocal = localStorage.getItem("contact");
+    return checkLocal ? JSON.parse(checkLocal) : initialContactDetails;
+  });
+
+  useEffect(() => {
+    localStorage.setItem("contact", JSON.stringify(contactDetails));
+    localStorage.setItem("name", fullName);
+  }, [fullName, contactDetails]);
 
   function handleName(e) {
     setFullName(e.target.value);
@@ -28,7 +42,7 @@ function Header() {
   return (
     <div id="header">
       <div id="name">
-        <input type="text" id="fullName" placeholder={fullName} onChange={handleName} />
+        <input type="text" id="fullName" value={fullName} onChange={handleName} />
       </div>
       <div id="contact">
         {contactDetails.map((item) => {
