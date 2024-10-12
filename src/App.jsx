c24e8b06834e9d0f12661b2cd4b988a242179008
elisha <elishamutang@outlook.com>
2024-10-12 15:57:47 +0800

    [update] Removed empty line.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import Header from "./components/Header";
 import MainContent from "./components/MainContent";
 import MainNav from "./components/MainNavBar";
-
 import "./styles/MainNavBar.css";
 import "./styles/Section.css";
 import "./styles/Education.css";
