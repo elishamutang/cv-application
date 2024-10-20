b680290327204cc09e122e57b5934b9b6c86104d
elisha <elishamutang@outlook.com>
2024-10-20 16:39:29 +0800

    [update] Added Footer component.

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+function Footer() {
+  return (
+    <div className="footer">
+      Made by{" "}
+      <a href="https://elishamutang.netlify.app/" target="_blank">
+        elishamutang
+      </a>
+    </div>
+  );
+}
+
+export default Footer;
