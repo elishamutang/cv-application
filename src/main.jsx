aa574e5119f2d53e58bb08438dc3fb3fb31d2a27
elisha <elishamutang@outlook.com>
2024-10-23 21:40:56 +0800

    [update] Re-factor code to utilise JSX declaration instead of using objects.

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,22 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
+import { createBrowserRouter, createRoutesFromElements, RouterProvider, Route } from "react-router-dom";
+import Root from "./routes/Root.jsx";
+import ViewPDF from "./routes/ViewPDF.jsx";
 import "./styles/index.css";
+
+// JSX declaration
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <>
+      <Route element={<Root />} path="/" />
+      <Route element={<ViewPDF />} path="ViewPDF" />
+    </>
+  )
+);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>
 );
