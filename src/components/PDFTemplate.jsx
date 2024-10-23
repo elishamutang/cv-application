ee5f83bbedc6330d7ac8a037f045e1803fbf1de3
elisha <elishamutang@outlook.com>
2024-10-23 23:07:39 +0800

    [update] Updated PDF template.

--- src/components/PDFTemplate.jsx
+++ src/components/PDFTemplate.jsx
@@ -30,13 +30,30 @@ export default function PDFTemplate() {
     section: {
       display: "flex",
       textAlign: "center",
-      // margin: ""
       width: "100%",
+    },
+    sectionHeader: {
+      fontSize: "24px",
+      fontWeight: "bold",
     },
     secondLevel: {
       display: "flex",
       flexDirection: "row",
       justifyContent: "space-between",
+      fontSize: "14.4px",
+      fontWeight: "bold",
+      fontFamily: "Helvetica-Bold",
+    },
+    thirdLevel: {
+      alignItems: "flex-start",
+      marginTop: "8px",
+      marginBottom: "8px",
+      lineHeight: 1.5,
+    },
+    bulletPoint: {
+      display: "flex",
+      flexDirection: "row",
+      fontSize: "14.4px",
     },
   });
 
@@ -51,13 +68,23 @@ export default function PDFTemplate() {
         {JSON.parse(localStorage.getItem("education")).map((item) => {
           return (
             <View key={item.id} style={[styles.section, { marginTop: 32 }]}>
-              <Text style={styles.sectionHeader}>Education</Text>
+              <Text style={[styles.name, styles.sectionHeader]}>Education</Text>
               <View style={styles.secondLevel}>
                 <Text style={[styles.organisation, styles.left]}>{item.institution}</Text>
                 <View style={styles.right}>
                   <Text style={styles.location}>{item.location}</Text>
                   <Text style={styles.date}>{item.graduationDate}</Text>
                 </View>
+              </View>
+              <View style={styles.thirdLevel}>
+                {item.content.map((content) => {
+                  return (
+                    <View style={styles.bulletPoint} key={content.id}>
+                      <Text>•</Text>
+                      <Text> {content.value}</Text>
+                    </View>
+                  );
+                })}
               </View>
             </View>
           );
