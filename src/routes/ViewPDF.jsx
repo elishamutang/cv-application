43c7bf2f734f7718148e38eb4e79b98db934bb5e
elisha <elishamutang@outlook.com>
2024-10-31 23:51:05 +0800

    [update] Removed comments.

--- src/routes/ViewPDF.jsx
+++ src/routes/ViewPDF.jsx
@@ -7,11 +7,6 @@ export default function ViewPDF() {
       border: "none",
       width: "100%",
       height: "100%",
-      // Wait until author has addressed media query issue raised on 23/10/24.
-      // "@media min-width: 800px": {
-      //   width: "100%",
-      //   height: "100%",
-      // },
     },
   });
 
