c78fc047c9127bc1b7ee568d07a5e37fdea503d2
elisha <elishamutang@outlook.com>
2024-11-03 15:05:49 +0800

    [update] Sections are rendered based on the ordering set in Editing mode.

--- src/components/PDFTemplate.jsx
+++ src/components/PDFTemplate.jsx
@@ -91,84 +91,109 @@ export default function PDFTemplate() {
           <Text style={styles.text}>{JSON.parse(localStorage.getItem("contact"))[0].field}</Text>
           <Text style={[styles.text, styles.borderOn]}>{JSON.parse(localStorage.getItem("contact"))[1].field}</Text>
         </View>
-        {/* Education Section only */}
-        {/* Education Header */}
-        <Text style={[styles.name, styles.sectionHeader, { marginTop: 8 }]}>Education</Text>
-        {JSON.parse(localStorage.getItem("education")).map((item) => {
-          return (
-            <View key={item.id} style={styles.section}>
-              <View style={styles.secondLevel}>
-                <Text style={styles.organisation}>{item.institution}</Text>
-                <View>
-                  <Text style={styles.location}>{item.location}</Text>
-                  <Text style={styles.date}>{item.graduationDate}</Text>
-                </View>
-              </View>
-              <View style={styles.thirdLevel}>
-                {item.content.map((content) => {
+
+        {/* Sections will be printed out based on the order that was set in Editing mode. */}
+        {JSON.parse(localStorage.getItem("order")).map((item) => {
+          if (item.title === "Education") {
+            return (
+              // Education section
+              <>
+                {/* Education header */}
+                <Text style={[styles.name, styles.sectionHeader, { marginTop: 8 }]}>Education</Text>
+                {/* Education content */}
+                {JSON.parse(localStorage.getItem("education")).map((item) => {
                   return (
-                    <View style={styles.bulletPoint} key={content.id}>
-                      <Text>• {content.value}</Text>
+                    <View key={item.id} style={styles.section}>
+                      <View style={styles.secondLevel}>
+                        <Text style={styles.organisation}>{item.institution}</Text>
+                        <View>
+                          <Text style={styles.location}>{item.location}</Text>
+                          <Text style={styles.date}>{item.graduationDate}</Text>
+                        </View>
+                      </View>
+                      <View style={styles.thirdLevel}>
+                        {item.content.map((content) => {
+                          return (
+                            <View style={styles.bulletPoint} key={content.id}>
+                              <Text>• {content.value}</Text>
+                            </View>
+                          );
+                        })}
+                      </View>
                     </View>
                   );
                 })}
-              </View>
-            </View>
-          );
-        })}
-        {/* Sections */}
-        {/* Section Header */}
-        <Text style={[styles.name, styles.sectionHeader]}>Experience</Text>
-        {JSON.parse(localStorage.getItem("sections")).map((section) => {
-          return (
-            // Main container
-            <View key={section.id}>
-              {section.content.map((item) => {
-                return (
-                  // Section container
-                  <View key={item.id} wrap={false} style={styles.section}>
-                    {/* Second level */}
-                    <View style={[styles.secondLevel, { marginBottom: "3px" }]}>
-                      <View>
-                        <Text style={styles.organisation}>{item.organisation}</Text>
-                        <Text style={styles.position}>{item.position}</Text>
+              </>
+            );
+          } else if (item.title === "Skills And Interests") {
+            // Skills And Interests
+            return (
+              <>
+                {/* Header */}
+                <Text style={[styles.name, styles.sectionHeader]}>Skills And Interests</Text>
+
+                {/* Content */}
+                <View style={[styles.section]}>
+                  {JSON.parse(localStorage.getItem("skillsAndInterests")).info.map((item) => {
+                    return (
+                      <View key={item.id} style={styles.skillsAndInterests}>
+                        <Text style={styles.heading}>{item.heading}</Text>
+                        <Text style={{ width: "100%", textAlign: "left" }}>{item.value}</Text>
                       </View>
-                      <View>
-                        <Text style={styles.location}>{item.location}</Text>
-                        <Text style={styles.date}>
-                          {item.startDate} - {item.endDate}
-                        </Text>
-                      </View>
-                    </View>
-                    {/* Third level */}
-                    <View style={styles.thirdLevel}>
-                      {item.bulletPoints.map((point) => {
+                    );
+                  })}
+                </View>
+              </>
+            );
+          } else {
+            // Sections
+            return (
+              <>
+                {/* Section Header */}
+                <Text style={[styles.name, styles.sectionHeader]}>Experience</Text>
+
+                {/* Section Content */}
+                {JSON.parse(localStorage.getItem("sections")).map((section) => {
+                  return (
+                    // Main container
+                    <View key={section.id}>
+                      {section.content.map((item) => {
                         return (
-                          <View style={styles.bulletPoint} key={point.id}>
-                            <Text>• {point.value}</Text>
+                          // Section container
+                          <View key={item.id} wrap={false} style={styles.section}>
+                            {/* Second level */}
+                            <View style={[styles.secondLevel, { marginBottom: "3px" }]}>
+                              <View>
+                                <Text style={styles.organisation}>{item.organisation}</Text>
+                                <Text style={styles.position}>{item.position}</Text>
+                              </View>
+                              <View>
+                                <Text style={styles.location}>{item.location}</Text>
+                                <Text style={styles.date}>
+                                  {item.startDate} - {item.endDate}
+                                </Text>
+                              </View>
+                            </View>
+                            {/* Third level */}
+                            <View style={styles.thirdLevel}>
+                              {item.bulletPoints.map((point) => {
+                                return (
+                                  <View style={styles.bulletPoint} key={point.id}>
+                                    <Text>• {point.value}</Text>
+                                  </View>
+                                );
+                              })}
+                            </View>
                           </View>
                         );
                       })}
                     </View>
-                  </View>
-                );
-              })}
-            </View>
-          );
-        })}
-        {/* Skills And Interests */}
-        {/* Header */}
-        <Text style={[styles.name, styles.sectionHeader]}>Skills And Interests</Text>
-        <View style={[styles.section]}>
-          {JSON.parse(localStorage.getItem("skillsAndInterests")).info.map((item) => {
-            return (
-              <View key={item.id} style={styles.skillsAndInterests}>
-                <Text style={styles.heading}>{item.heading}</Text>
-                <Text style={{ width: "100%", textAlign: "left" }}>{item.value}</Text>
-              </View>
+                  );
+                })}
+              </>
             );
-          })}
-        </View>
+          }
+        })}
       </Page>
     </Document>
   );
